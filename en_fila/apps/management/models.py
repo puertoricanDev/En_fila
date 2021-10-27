e21e8693128eaefc1affe9a9e37009ea760fc1f0
leny133 <63651131+leny133@users.noreply.github.com>
2021-10-27 16:59:02 -0400

    management app models and views

--- en_fila/apps/management/models.py
+++ en_fila/apps/management/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from ..premium.models import Suscribed
 # Create your models here.
+
+class Owner_areas(models.Model):
+    owner = models.ForeignKey("Suscribed", on_delete=models.CASCADE)
+    place_name = models.CharField(max_length=45)
+    place_id = models.IntegerField(blank=False)
--- en_fila/apps/management/views.py
+++ en_fila/apps/management/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from en_fila.apps.management.models import Owner_areas
+from ..mi_fila.models import *
+from ..premium.models import *
 # Create your views here.
 def managementIndex(request):
+    if request.user.is_authenticated:
+        return managementpages(request)
+    else:
+        return render(request, "premium/login.html")
+
+def managementpages(request):
+    if request.method == "POST":
+        owner = request.user
+        
+        for x in range(4):
+            owner_areas = Owner_areas.objects.get(owner = owner,place_id = x)
+            if Owner_areas.DoesNotExist:
+                place_name = request.POST["place"+x]
+                owner_areas = Owner_areas(
+                        owner = owner,
+                        place_name = place_name,
+                        places_id = x
+                    )
+            else:
+                owner_areas.place_name = place_name
+            owner_areas.save()
+    else:
+        return JsonResponse({"error": "POST request required."}, status=400)
     return render(request, "management/index.html")
